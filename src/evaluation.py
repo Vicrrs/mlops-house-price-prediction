c792d3ef430ef6d4a62e5cd1e05626da449ae4b6
Vicrrs <enricofermi2019@gmail.com>
2024-11-13 13:22:51 -0400

    logando previsoes no mlflow

--- src/evaluation.py
+++ src/evaluation.py
@@ -3,7 +3,7 @@ import mlflow
 from sklearn.metrics import r2_score, mean_absolute_error
 from joblib import load
 from src.logger import logger
-
+import pandas as pd
 
 def evaluate_model(model_path, X_test, y_test):
     logger.info(f"Carregando modelo para avaliação: {model_path}")
@@ -16,6 +16,16 @@ def evaluate_model(model_path, X_test, y_test):
     with mlflow.start_run():
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
+
+        # Logando previsões no MLflow
+        predictions_df = pd.DataFrame({
+            "expected": y_test,
+            "predicted": predictions
+        })
+        predictions_csv = "predictions.csv"
+        predictions_df.to_csv(predictions_csv, index=False)
+        mlflow.log_artifact(predictions_csv, artifact_path="predictions")
+
         mlflow.end_run()
 
     return r2, mae
